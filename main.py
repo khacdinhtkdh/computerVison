117bd323d4d0045ec27b60d558ee56e30a37a928
khacdinhtkdh <khacdinhtkdh@gmail.com>
2022-06-27 20:23:31 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+img = cv2.imread('DATA/00-puppy.jpg')
+
+print(type(img))
+print(img.shape)
+
+# normal RGB
+# open cv BGR
+# plt.imshow(img)
+# plt.show()
+
+# need convert BGR -> RGB
+
+
+
+
